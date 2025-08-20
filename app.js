ddb333f5f3ad273bffa07717a0999425942570ad
t-collab-lab <taviarobb@gmail.com>
2025-08-20 16:22:33 -0400

    Update app.js

--- app.js
+++ app.js
@@ -83,7 +83,7 @@ form.addEventListener('submit', function(e) {
   };
 
   entries.push(data);
-  saveEntries();
+  // saveEntries();
   sendToGoogleSheet(data);
   renderEntries();
   renderChart();
